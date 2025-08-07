d93ed0741cb791d4d68f82229b5575d12bf6bc73
Khurshid Alam <khurshidalam741@gmail.com>
2025-08-07 06:13:56 -0700

    Initial commit

---
+++ src/MathUtils.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace YourName\MathUtils;
+
+class MathUtils
+{
+    public static function sum($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public static function minus($a, $b)
+    {
+        return $a - $b;
+    }
+
+    public static function gun($a, $b)
+    {
+        return $a * $b;
+    }
+
+    public static function vag($a, $b)
+    {
+        return $b != 0 ? $a / $b : null;
+    }
+}
---
+++ src/MathUtilsServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace YourName\MathUtils;
+
+use Illuminate\Support\ServiceProvider;
+
+class MathUtilsServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
